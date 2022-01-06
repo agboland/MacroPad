400660a938b2de543acddb5c8474aae41e72397e
agboland <65364090+agboland@users.noreply.github.com>
2022-01-05 22:31:50 -0500

    All files on pico at time of working order

---
+++ code.py
@@ -0,0 +1,84 @@
+import time
+import usb_hid
+from adafruit_hid.keycode import Keycode
+from adafruit_hid.keyboard import Keyboard
+import board
+import digitalio
+# These are the corresponding GPIOs on the Pi Pico
+# that you soldered
+btn1_pin = board.GP15
+btn2_pin = board.GP14
+btn3_pin = board.GP13
+btn4_pin = board.GP12
+btn5_pin = board.GP11
+btn6_pin = board.GP10
+btn7_pin = board.GP9
+btn8_pin = board.GP8
+btn9_pin = board.GP7
+btn1 = digitalio.DigitalInOut(btn1_pin)
+btn1.direction = digitalio.Direction.INPUT
+btn1.pull = digitalio.Pull.DOWN
+btn2 = digitalio.DigitalInOut(btn2_pin)
+btn2.direction = digitalio.Direction.INPUT
+btn2.pull = digitalio.Pull.DOWN
+btn3 = digitalio.DigitalInOut(btn3_pin)
+btn3.direction = digitalio.Direction.INPUT
+btn3.pull = digitalio.Pull.DOWN
+btn4 = digitalio.DigitalInOut(btn4_pin)
+btn4.direction = digitalio.Direction.INPUT
+btn4.pull = digitalio.Pull.DOWN
+btn5 = digitalio.DigitalInOut(btn5_pin)
+btn5.direction = digitalio.Direction.INPUT
+btn5.pull = digitalio.Pull.DOWN
+btn6 = digitalio.DigitalInOut(btn6_pin)
+btn6.direction = digitalio.Direction.INPUT
+btn6.pull = digitalio.Pull.DOWN
+btn7 = digitalio.DigitalInOut(btn7_pin)
+btn7.direction = digitalio.Direction.INPUT
+btn7.pull = digitalio.Pull.DOWN
+btn8 = digitalio.DigitalInOut(btn8_pin)
+btn8.direction = digitalio.Direction.INPUT
+btn8.pull = digitalio.Pull.DOWN
+btn9 = digitalio.DigitalInOut(btn9_pin)
+btn9.direction = digitalio.Direction.INPUT
+btn9.pull = digitalio.Pull.DOWN
+keyboard = Keyboard(usb_hid.devices)
+# below are the key values that you can change to
+# fit your preferences. Change Keycode.ONE for example to
+# (Keycode.CONTROL, Keycode.F4) for CTRL + F4
+# on the first button.
+# See the official CircuitPython docs
+# for additional help
+while True:
+    if btn1.value:
+        keyboard.send(Keycode.ESCAPE,Keycode.LEFT_CONTROL,Keycode.S)
+        time.sleep(0.5)
+        keyboard.send(Keycode.H,Keycode.E,Keycode.L,)
+        time.sleep(0.1)
+        keyboard.send(Keycode.L,Keycode.O)
+        time.sleep(0.2)
+    if btn2.value:
+        keyboard.send(Keycode.UP_ARROW)
+        time.sleep(0.1)
+    if btn3.value:
+        keyboard.send(Keycode.SEVEN)
+        time.sleep(0.1)
+    if btn4.value:
+        keyboard.send(Keycode.TWO)
+        time.sleep(0.1)
+    if btn5.value:
+        keyboard.send(Keycode.FIVE)
+        time.sleep(0.1)
+    if btn6.value:
+        keyboard.send(Keycode.EIGHT)
+        time.sleep(0.1)
+    if btn7.value:
+        keyboard.send(Keycode.THREE)
+        time.sleep(0.1)
+    if btn8.value:
+        keyboard.send(Keycode.SIX)
+        time.sleep(0.1)
+    if btn9.value:
+        keyboard.send(Keycode.NINE)
+        time.sleep(0.1)
+        time.sleep(0.1)
